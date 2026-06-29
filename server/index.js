27eb71d00f56e925ab077b6d2313e146369598f6
Stanley Bankesie <stanleyetornam@gmail.com>
2026-06-29 02:16:03 +0000

    fix: add demo.omnisuite-erp.com to CORS allowed origins

--- server/index.js
+++ server/index.js
@@ -116,7 +116,7 @@ const allowedOrigins = (() => {
     "http://127.0.0.1:5173",
     "http://localhost:5174",
     "http://127.0.0.1:5174",
-    // "https://demo.omnisuite-erp.com",
+    "https://serianamart.omnisuite-erp.com",
     "https://demo.omnisuite-erp.com",
   ];
 })();
